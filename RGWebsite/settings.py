ffd36cc8cdb81d57d0f88bba27b292c82cf42050
Daniel Duffy <dmd9042@gmail.com>
2022-03-07 17:16:12 -0500

    Random updates throughout the day trying to tie together the front end

--- RGWebsite/settings.py
+++ RGWebsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'storis.apps.StorisConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
